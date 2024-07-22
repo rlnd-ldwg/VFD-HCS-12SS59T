9278cf8c597c7ecb2750caa5d3e6e758a1af2c5d
Ludwig, Roland <roland.ludwig@deutsche-finanzagentur.de>
2024-07-22 14:52:00 +0200

    new vfd library

---
+++ FW/include/vfd.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   vfd.h
+ * Author: roland
+ *
+ * Created on July 20, 2024, 6:46 PM
+ */
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+    
+#ifndef VFD_H
+#define VFD_H
+
+
+#endif  /* VFD_H */
+
+#ifdef  __cplusplus
+}
+#endif
